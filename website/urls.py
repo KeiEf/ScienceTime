4e858ce64abe34ce2b88ac066eb5b38213457acf
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-11-28 21:12:15 +0900

    reset

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views 
-from .views import TestView, HomeView
+from .views import TestView, HomeView, , PostDetailView
 
 urlpatterns = [
 	path('', HomeView.as_view(), name="index"),
 	path('test', TestView.as_view(), name="test"),	
+    path('post/<int:pk>', PostDetailView.as_view(), name="post_detail"),
     ]
 
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Product, Post, Genre, Category
 
 
 # Create your views here.
@@ -19,5 +19,27 @@ class HomeView(ListView):
         context={}
         post_list = Post.objects.all()
         context["post_list"] = post_list
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_details.html'
+
+    def get(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, id=self.kwargs['pk'])
+        post.views += 1
+        post.save()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(PostDetailView, self).get_context_data(*args, **kwargs)
+        popular_list = Post.objects.filter(state="published").order_by('-views')   
+        popular_items = Product.objects.order_by('-views')                   
+        context["related_posts"] = self.object.post_tags.similar_objects()[:3]
+        context["popular_list"] = popular_list
+        context["popular_items"] = popular_items        
+        context["cat_menu"] = cat_menu
         return context
 
