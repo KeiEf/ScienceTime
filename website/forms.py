414bd295bdb1d147794aefa2828c8fff57d47628
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-11-28 21:22:10 +0900

    reset

---
+++ website/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import Post, Product, Genre, Category
+
+#choices = [('sports','sports'),('entertainment','entertainment')]
+choices = Category.objects.all().values_list('name','name')
+choice_list = []
+
+state_list = [('published','published'),('private','private')]
+
+for item in choices:
+    choice_list.append(item)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','image_url', 'abstract', 'author', 'category', 'post_tags','content', 'post_date', 'header_image', 'state')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'abstract': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'elder', 'type':'hidden'}),
+        #    'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'state': forms.Select(choices=state_list, attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+  
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','image_url','abstract',  'category', 'post_tags', 'content', 'post_date', 'header_image', 'state')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Thi is Title PlaceHolder'}),
+            'abstract': forms.TextInput(attrs={'class': 'form-control'}),
+            'state': forms.Select(choices=state_list, attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
+        
--- website/views.py
+++ website/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Product, Post, Genre, Category
-
+from .forms import PostForm, EditForm
+from django.db.models import Max, Case, When, Sum, Count, Q, F
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -17,9 +19,92 @@ class HomeView(ListView):
 
    def get_context_data(self):
         context={}
+        product_list = Product.objects.all()
         post_list = Post.objects.all()
+
+        context["product_list"] = product_list
         context["post_list"] = post_list
         return context
+#   get_latest_entries(self):
+ #     latest_news = Post.object.filer
+
+class PostView(ListView):
+   model = Post
+   template_name = 'posts.html'
+
+   def get_context_data(self, *args, **kwargs):
+
+        context={}
+        object_list = Post.objects.filter(state="published").order_by('-post_date')
+        popular_list = Post.objects.filter(state="published").order_by('-views')      
+
+        phys_list = Post.objects.filter(category="physics and astronomy", state="published").order_by('-post_date')
+        phys_latest = Post.objects.filter(category="physics and astronomy", state="published").latest('post_date')
+
+        env_list = Post.objects.filter(category="environment", state="published").order_by('-post_date')
+        env_latest = Post.objects.filter(category="environment", state="published").latest('post_date')
+
+        health_list = Post.objects.filter(category="medical and health", state="published").order_by('-post_date')
+        health_latest = Post.objects.filter(category="medical and health", state="published").latest('post_date')
+
+        cat_menu = Category.objects.all()
+        cat_menu = super(PostView, self).get_context_data(*args, **kwargs)
+        cat_menu_list = Category.objects.all()
+
+        context["cat_menu"] = cat_menu
+        context["cat_menu_list"] = cat_menu_list
+
+        context["object_list"] = object_list
+        context["popular_list"] = popular_list
+
+        context["phys_list"] = phys_list
+        context["phys_latest"] = phys_latest
+        context["env_list"] = env_list
+        context["env_latest"] = env_latest  
+        context["health_list"] = health_list
+        context["health_latest"] = health_latest              
+        return context
+
+class AllPostView(ListView):
+   model = Post
+   template_name = 'post_all.html'
+   paginate_by = 20
+   def get_queryset(self):
+       object_list =  Post.objects.filter(state="published").order_by('-post_date')
+       return object_list
+
+class PostTagView(ListView):
+    model = Post
+    template_name ='post_all.html'
+    paginate_by = 20
+
+    def get_context_data(self, *args, **kwargs):
+        context = {}
+        object_list = Post.objects.filter(post_tags__slug=self.kwargs.get('tag_slug'))
+        cat_menu = Category.objects.all()
+        popular_list = Post.objects.filter(state="published").order_by('-views')   
+        popular_items = Product.objects.order_by('-views') 
+
+        context["popular_list"] = popular_list
+        context["popular_items"] = popular_items        
+        context["cat_menu"] = cat_menu
+        context["object_list"] = object_list           
+        return context
+
+def CategoryView(request, cats):
+    paginate_by = 20
+    cat_menu = Category.objects.all()
+    category_posts = Post.objects.filter(category=cats.replace('-',' '), state="published")
+    total = category_posts.count()
+    popular_list = Post.objects.filter(state="published").order_by('-views')   
+    popular_items = Product.objects.order_by('-views')                   
+    return render(request, 'category.html', {
+        'cats':cats.title().replace('-',' '), 
+        'category_posts':category_posts, 
+        'cat_menu':cat_menu,
+        'popular_list': popular_list,
+        'popular_items': popular_items   
+        })
 
 
 class PostDetailView(DetailView):
@@ -42,4 +127,98 @@ class PostDetailView(DetailView):
         context["popular_items"] = popular_items        
         context["cat_menu"] = cat_menu
         return context
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+
+    def form_valid(self, form):
+        firma = form.save()
+        return redirect('post_detail', firma.pk)    
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+
+    def get_success_url(self):
+        #messages.success(self.request, '投稿を編集しました。')
+        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['pk']})
+
+###########
+
+class ProductView(ListView):
+   model = Product
+   template_name = 'products.html'
+   paginate_by = 30
+
+   def get_context_data(self, *args, **kwargs):
+
+        context={}
+        object_list = Product.objects.all().order_by('-post_date')
+        popular_list = Product.objects.all().order_by('-views')      
+
+        fashion_list = Product.objects.filter(genre="fashion").order_by('-post_date')
+        toy_list = Product.objects.filter(genre="toy").order_by('-post_date')
+        merch_list = Product.objects.filter(genre="merch").order_by('-post_date')
+
+        genre_menu = Genre.objects.all()
+      #  genre_menu_list = Category.objects.all()
+
+        context["genre_menu"] = genre_menu
+        context["object_list"] = object_list
+        context["popular_list"] = popular_list
+
+        context["fashion_list"] = fashion_list
+        context["toy_list"] = toy_list 
+        context["merch_list"] = merch_list             
+        return context
+
+
+def GenreView(request, gens):
+    paginate_by = 20
+    genre_menu = Genre.objects.all()
+    genre_posts = Product.objects.filter(genre=gens.replace('-',' ')) 
+    popular_items = Product.objects.order_by('-views') 
+    fashion_genre = {'T-shirts' : 'T-shirts', 'hoodie': 'hoodie' }
+    toy_genre = {'mechanics' : 'mechanics', 'maths_and_others': 'maths_and_others' }             
+    return render(request, 'genre.html', {
+        'gens':gens.title().replace('-',' '), 
+        'genre_posts':genre_posts, 
+        'genre_menu':genre_menu,
+        'popular_items': popular_items,
+        'fashion_genre': fashion_genre,
+        'toy_genre': toy_genre        
+        })
+
+'''
+   def get_context_data(self):
+        context={}
+        toy_list = Product.objects.filter(category="toy")
+        T_list = Product.objects.filter(category="T-shirts")
+
+        context["toy_list"] = toy_list
+        context["T_list"] = T_list
+        return context
+'''
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_details.html'
+
+    def get(self, request, *args, **kwargs):
+        product = get_object_or_404(Product, id=self.kwargs['pk'])
+        product.views += 1
+        product.save()
+        return super().get(request, *args, **kwargs)
+
+class TagIndexView(ListView):
+    model = Product
+    template_name ='genre.html'
+    context_object_name = 'product'
+
+    def get_queryset(self):
+        return Product.objects.filter(tags__slug=self.kwargs.get('tag_slug'))
 
