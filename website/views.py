7c670cb6d63c76c64fbad668a0d696223a593066
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-11-29 20:46:18 +0900

    xxx

--- website/views.py
+++ website/views.py
@@ -155,8 +155,8 @@ class PostDetailView(DetailView):
         cat_menu = Category.objects.all()
         context = super(PostDetailView, self).get_context_data(*args, **kwargs)
         popular_list = Post.objects.filter(state="published").order_by('-views')   
-        popular_items = Product.objects.order_by('-views')                  
-        context["related_posts"] = self.object.post_tags.similar_objects()[:5]
+        popular_items = Product.objects.order_by('-views')              
+        context["related_posts"] = self.object.post_tags.similar_objects()
         context["popular_list"] = popular_list
         context["popular_items"] = popular_items        
         context["cat_menu"] = cat_menu
