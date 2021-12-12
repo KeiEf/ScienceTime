06c5900e0f20dad046cf9210675f06e8bc41d66c
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-12-12 10:35:35 +0900

    note

--- website/templates/nav.php
+++ website/templates/nav.php
@@ -33,7 +33,8 @@
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
               <li><a class="dropdown-item" href="#">全て</a></li>
-              <li><a class="dropdown-item" href="{% url 'subject' 'physics' %}">物理</a></li>              
+              <li><a class="dropdown-item" href="{% url 'subject' 'maths' %}">数学</a></li> 
+              <li><a class="dropdown-item" href="{% url 'subject' 'physics' %}">物理</a></li>          
             </ul>
           </li>
 
