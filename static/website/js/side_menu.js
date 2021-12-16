f89e45b87274f708bad3c784fec6660a68961d51
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-12-16 22:18:39 +0900

    note

--- static/website/js/side_menu.js
+++ static/website/js/side_menu.js
@@ -1,5 +1,17 @@
 $(function() {
-    $('.container').click(function() {
+    $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+});
+
+$(function() {
+    $('.note_content').click(function() {
         $(this).toggleClass('active');
  
         if ($(this).hasClass('active')) {
