7cfc76e500b4a81278cfb593925821ff8c5c7e53
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-12-17 10:47:48 +0900

    sort

---
+++ website/templates/php/post_sort_btn.php
@@ -0,0 +1,11 @@
+<div class="select_area">
+  <form method="GET" action="">
+    <select class="post_sort_btn" name="sort" method="GET" onChange="submit(this.form)">
+      <option>並び替え</option>
+      <option value="date">投稿日（新しい）</option>
+      <option value="inv_date">投稿日（古い）</option>             
+      <option value="view">閲覧数（多い）</option>
+      <option value="inv_view">閲覧数（少ない）</option>
+    </select>
+  </form>
+</div>
