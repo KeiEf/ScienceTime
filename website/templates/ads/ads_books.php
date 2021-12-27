6914155b7bf51521b6a507c9b80614f6c0d4b204
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-12-27 17:03:47 +0900

    ad

--- website/templates/ads/ads_books.php
+++ website/templates/ads/ads_books.php
@@ -46,5 +46,11 @@
 </script>
 
 {% else %}
-   {% include 'ads/ads_vert.php' %}
+<div class="pc_area">
+    <script type="text/javascript" src="//rot8.a8.net/jsa/ffe20b5e1d34af433378dac471206fd3/dc5c7986daef50c1e02ab09b442ee34f.js"></script>
+</div>
+         
+<div class="phone_area">
+   <script type="text/javascript" src="//rot0.a8.net/jsa/ffe20b5e1d34af433378dac471206fd3/d2490f048dc3b77a457e3e450ab4eb38.js"></script>
+</div>
 {% endif %}
