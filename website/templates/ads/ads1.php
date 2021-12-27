dc42ecefa030627133d22fdc1918c34b12d3f132
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-12-27 16:07:43 +0900

    ad

--- website/templates/ads/ads1.php
+++ website/templates/ads/ads1.php
@@ -1,1 +1,16 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7524603691208984"
+     crossorigin="anonymous"></script>
+<!-- STスクエア -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7524603691208984"
+     data-ad-slot="7285949764"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<!--
 <script type="text/javascript" src="//rot7.a8.net/jsa/ffe20b5e1d34af433378dac471206fd3/af032fbcb07ffc7bd2569d86ae4ce1f5.js"></script>
+-->
--- website/templates/ads/ads_vert.php
+++ website/templates/ads/ads_vert.php
@@ -1,3 +1,17 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7524603691208984"
+     crossorigin="anonymous"></script>
+<!-- ST横長 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7524603691208984"
+     data-ad-slot="6000788254"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<!--
 <div class="pc_area">
     <script type="text/javascript" src="//rot8.a8.net/jsa/ffe20b5e1d34af433378dac471206fd3/dc5c7986daef50c1e02ab09b442ee34f.js"></script>
 </div>
@@ -5,3 +19,4 @@
 <div class="phone_area">
    <script type="text/javascript" src="//rot0.a8.net/jsa/ffe20b5e1d34af433378dac471206fd3/d2490f048dc3b77a457e3e450ab4eb38.js"></script>
 </div>
+-->
