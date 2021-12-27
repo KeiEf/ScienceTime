ad83fd9d36c849ad12ea625a3893d371ea998b39
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-12-27 16:38:56 +0900

    ad

--- website/templates/ads/ads_vert.php
+++ website/templates/ads/ads_vert.php
@@ -6,11 +6,10 @@
      data-ad-client="ca-pub-7524603691208984"
      data-ad-slot="6000788254"
      data-ad-format="auto"
-     data-full-width-responsive="false"></ins>
+     data-full-width-responsive="true"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
-
 <!--
 <div class="pc_area">
     <script type="text/javascript" src="//rot8.a8.net/jsa/ffe20b5e1d34af433378dac471206fd3/dc5c7986daef50c1e02ab09b442ee34f.js"></script>
