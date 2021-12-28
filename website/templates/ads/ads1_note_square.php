1a86a3fdcc38dcfc3bc275a07b1c62aa2d5f5a69
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-12-28 11:32:36 +0900

    pop

---
+++ website/templates/ads/ads1_note_square.php
@@ -0,0 +1,18 @@
+<div class="pc_area">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7524603691208984"
+     crossorigin="anonymous"></script>
+<!-- STスクエア -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7524603691208984"
+     data-ad-slot="7285949764"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+         
+<div class="phone_area">
+<script type="text/javascript" src="//rot1.a8.net/jsa/ffe20b5e1d34af433378dac471206fd3/0e51011a4c4891e5c01c12d85c4dcaa7.js"></script>
+</div>
