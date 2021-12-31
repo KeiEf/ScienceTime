619dcfd4f203dd57219982bb55768bfb4ea91476
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2021-12-31 16:13:45 +0900

    ads

---
+++ website/templates/ads/ads_free.php
@@ -0,0 +1,12 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7524603691208984"
+     crossorigin="anonymous"></script>
+<!-- ST横長 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7524603691208984"
+     data-ad-slot="6000788254"
+     data-ad-format="horizontal" 
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
