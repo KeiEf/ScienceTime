c1582ac4d40856dd29ee68d5ebb3dd1862c76ed9
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2022-01-05 20:26:07 +0900

    eq

--- website/templatetags/replace.py
+++ website/templatetags/replace.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def blank_remove(value):
     return value.replace(" ","").replace("-","_")
+
+@register.filter
+def math_scroll(value):
+    return value.replace("\\begin{align}","<div class='eq'>\n\\begin{align}").replace("\\end{align}","\\end{align}</div>").replace("\\begin{equation}","<div class='eq'>\n\\begin{equation}").replace("\\end{equation}","\\end{equation}</div>")
