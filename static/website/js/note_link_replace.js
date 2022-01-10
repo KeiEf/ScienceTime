2985d375f511c9c2915756a3fe71e5ccf970d9aa
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2022-01-10 11:02:52 +0900

    note

--- static/website/js/note_link_replace.js
+++ static/website/js/note_link_replace.js
@@ -1,6 +1,9 @@
 for (var i = 0; i < 3; ++i) {
   var str = document.getElementById("note_id-" + i ).innerHTML;
 
+  var or3 = "Planckの放射式"
+  var rp3 = "<a href='https://www.sciencetime.jp/note/66' target='_blank'>" + or3 +"<\/a>"
+
   var or64 = "Hamilton-Jacobi方程式"
   var rp64 = "<a href='https://www.sciencetime.jp/note/66' target='_blank'>" + or64 +"<\/a>"
 
@@ -16,10 +19,10 @@ for (var i = 0; i < 3; ++i) {
   var or72 = "有効放射温度"
   var rp72 = "<a href='https://www.sciencetime.jp/note/72' target='_blank'>" + or72 +"<\/a>"
 
+  var or73 = "Stefan-Boltzmannの法則"
+  var rp73 = "<a href='https://www.sciencetime.jp/note/72' target='_blank'>" + or73 +"<\/a>"
 
-
-
-  var replaced = str.replaceAll(or66, rp66).replaceAll(or68,rp68).replaceAll(or69, rp69).replaceAll(or72,rp72); 
+  var replaced = str.replaceAll(or3, rp3).replaceAll(or64, rp64).replaceAll(or66, rp66).replaceAll(or68,rp68).replaceAll(or69, rp69).replaceAll(or72,rp72).replaceAll(or73, rp73); 
 //document.getElementsByTagName("p").innerHTML = replaced;
   document.getElementById("note_id-" + i).innerHTML = replaced;
 }
