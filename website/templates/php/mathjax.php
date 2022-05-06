ded1e8554d0c1dabdd310a5e77e3cc34edfa9517
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2022-05-06 10:25:19 +0900

    math

--- website/templates/php/mathjax.php
+++ website/templates/php/mathjax.php
@@ -25,8 +25,8 @@
               frh: ["{\\mathfrak{h}}"],
               frgl: ["{\\mathfrak{gl}}"],  
               frsu: ["{\\mathfrak{su}}"],
-              ad: ["{{\\rm ad}}"],
-              tr: ["{{\\rm tr}}"],
+              ad: ["{\\text{\\rm ad}}"],
+              tr: ["{\\text{\\rm tr}}"],
             }
     }     
     };
