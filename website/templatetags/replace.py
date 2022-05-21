9ba5cbdd9697c8d5ccfe10de45bf2cfc820b54e1
Kei Ef <symphony_of_virtuosity@yahoo.co.jp>
2022-05-21 14:55:17 +0900

    blue

--- website/templatetags/replace.py
+++ website/templatetags/replace.py
@@ -8,7 +8,7 @@ def blank_remove(value):
 
 @register.filter
 def math_scroll(value):
-    return value.replace("\\begin{align}","<div class='eq'>\n\\begin{align}").replace("\\end{align}","\\end{align}</div>").replace("\\begin{equation}","<div class='eq'>\n\\begin{equation}").replace("\\end{equation}","\\end{equation}</div>").replace("<b>","<span style='color:\#0000ff;font-weight:bold;'>").replace("</b>","</span>")
+    return value.replace("\\begin{align}","<div class='eq'>\n\\begin{align}").replace("\\end{align}","\\end{align}</div>").replace("\\begin{equation}","<div class='eq'>\n\\begin{equation}").replace("\\end{equation}","\\end{equation}</div>").replace("<b>","<span style='color:blue;font-weight:bold;'>").replace("</b>","</span>")
 
 @register.filter
 def html_replace(value):
